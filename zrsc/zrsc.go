cc91b92ce30f1b6770bf7523ea5d36bc909dfb80
Mahmud Ridwan <m@hjr265.me>
2014-05-14 16:56:10 +0600

    Impl HttpDir; resolves #2

--- zrsc/zrsc.go
+++ zrsc/zrsc.go
@@ -2,25 +2,49 @@ package zrsc
 
 import (
 	"archive/zip"
-	"io"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 )
 
 var (
 	initErr error
 
+	bs = map[string][]byte{}
 	fs = map[string]*zip.File{}
+	ds = map[string][]os.FileInfo{}
 )
 
+type HttpDir string
+
+func (d HttpDir) Open(name string) (http.File, error) {
+	return Open(strings.TrimLeft(path.Clean(path.Join(string(d), name)), "/"))
+}
+
 type File struct {
-	io.ReadCloser
+	*bytes.Reader
 
 	name string
 }
 
+func (f *File) Close() error {
+	return nil
+}
+
 func (f *File) Stat() (os.FileInfo, error) {
 	return fs[f.name].FileInfo(), nil
+}
+
+func (f *File) Readdir(count int) ([]os.FileInfo, error) {
+	d := ds[f.name]
+	if count > 0 && len(d) > count {
+		d = d[:count]
+	}
+	return d, nil
 }
 
 func Open(name string) (*File, error) {
@@ -33,9 +57,24 @@ func Open(name string) (*File, error) {
 		return nil, err
 	}
 
+	b, ok := bs[name]
+	if !ok {
+		b, err = ioutil.ReadAll(rc)
+		if err != nil {
+			return nil, err
+		}
+
+		err = rc.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		bs[name] = b
+	}
+
 	return &File{
-		ReadCloser: rc,
-		name:       name,
+		Reader: bytes.NewReader(b),
+		name:   name,
 	}, nil
 }
 
@@ -52,10 +91,10 @@ func init() {
 		}
 	}()
 
-	path, err := filepath.Abs(os.Args[0])
+	name, err := filepath.Abs(os.Args[0])
 	catch(err)
 
-	f, err := os.Open(path)
+	f, err := os.Open(name)
 	catch(err)
 	fi, err := f.Stat()
 	catch(err)
@@ -66,10 +105,13 @@ func init() {
 	for _, f := range r.File {
 		fi := f.FileInfo()
 		if fi.IsDir() {
-			continue
+			ds[f.Name] = []os.FileInfo{}
 		}
 
 		fs[f.Name] = f
+
+		dir := path.Dir(f.Name)
+		ds[dir] = append(ds[dir], fi)
 	}
 }
 
